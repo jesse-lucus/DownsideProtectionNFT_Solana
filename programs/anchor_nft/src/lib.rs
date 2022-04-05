fd9d33b07117df779541487695f477c6d1669e76
John31337 <95174336+John31337@users.noreply.github.com>
2022-04-05 22:18:32 +0800

    downside

--- programs/anchor_nft/src/lib.rs
+++
@@ -1,9 +0,0 @@
-
-#![allow(warnings)]
-use anchor_lang::prelude::*;
-pub mod entrypoint;
-pub mod processor;
-
-/// Prefix used in PDA derivations to avoid collisions with other programs.
-pub const PREFIX: &str = "ProtectedNFT";
-declare_id!("6qZt2RG8og8DKkjXFi3aQCWMg9pkiUUBb4s6nbGgMPry");
